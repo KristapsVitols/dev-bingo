f560d10efc2edfd38ea7ac8e40a4ba84b33b0833
kristapsvitols <kristapsvitols91@gmail.com>
2021-02-14 12:46:59 +0200

    slider lol

--- client/src/components/ShiftItem.vue
+++ client/src/components/ShiftItem.vue
@@ -1,21 +1,41 @@
 <template>
     <div>
-        <h4 class="mb-3">Current shift - {{ shift.name }}</h4>
+        <h4 class="mb-4">Current shift - {{ shift.name }}</h4>
         <div class="row">
-            <div v-for="spot in shift.spots" :key="spot.id" class="col-sm-4">
-                <div class="card p-3 mb-3" :class="getBgClass(spot)">
-                    <p>{{ spot.participant.name }}</p>
-                    <div class="d-flex">
-                        <div v-if="spot.type === 'morning'" v-b-tooltip.hover title="Morning shifts">
-                            <b-icon-sun variant="warning"/>
-                        </div>
-                        <div v-else v-b-tooltip.hover title="Evening shifts">
-                            <b-icon-moon variant="primary"/>
-                        </div>
-                        <span class="ml-3">{{ getFormattedDate(spot.date) }}</span>
-                    </div>
-                </div>
-            </div>
+            <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                controls
+                :interval="0"
+                img-width="512"
+                img-height="240"
+                indicators
+                class="mb-5 offset-sm-3 col-sm-6"
+                style="text-shadow: 1px 1px 2px #333;"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd">
+<!--                <div v-for="spot in shift.spots" :key="spot.id" class="col-sm-4">-->
+                    <b-carousel-slide
+                        v-for="spot in shift.spots"
+                        :key="spot.id"
+                        :caption="spot.participant.name"
+                        :text="getFormattedDate(spot.date)"
+                        img-src="img/nice-image.jpg"
+                    ></b-carousel-slide>
+<!--                </div>-->
+            </b-carousel>
+<!--                <div class="card p-3 mb-3" :class="getBgClass(spot)">-->
+<!--                    <p>{{ spot.participant.name }}</p>-->
+<!--                    <div class="d-flex">-->
+<!--                        <div v-if="spot.type === 'morning'" v-b-tooltip.hover title="Morning shifts">-->
+<!--                            <b-icon-sun variant="warning"/>-->
+<!--                        </div>-->
+<!--                        <div v-else v-b-tooltip.hover title="Evening shifts">-->
+<!--                            <b-icon-moon variant="primary"/>-->
+<!--                        </div>-->
+<!--                        <span class="ml-3">{{ getFormattedDate(spot.date) }}</span>-->
+<!--                    </div>-->
+<!--                </div>-->
         </div>
     </div>
 </template>
@@ -30,13 +50,30 @@ export default {
             required: true,
         }
     },
+    data: () => ({
+        slide: 0,
+        sliding: null,
+    }),
+    created() {
+        this.setCurrentSpot();
+    },
     methods: {
         // current spot in shift
-        getBgClass(spot) {
-            return moment(spot.date).isSame(moment(), 'day') ? 'bg-secondary' : '';
+        setCurrentSpot() {
+            this.shift.spots.forEach((spot, i) => {
+                if (moment(spot.date).isSame(moment(), 'day')) {
+                    this.slide = i;
+                }
+            });
         },
         getFormattedDate(date) {
             return moment(date).format('dddd, MMMM Do');
+        },
+        onSlideStart() {
+            this.sliding = true;
+        },
+        onSlideEnd() {
+            this.sliding = false;
         },
     },
 }
