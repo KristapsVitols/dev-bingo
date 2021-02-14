db9942d1e9f30de2b6f241d5113286d4a824bf7a
kristapsvitols <kristapsvitols91@gmail.com>
2021-02-14 14:41:11 +0200

    sorting

--- api/modules/participant/services/participant-service.js
+++ api/modules/participant/services/participant-service.js
@@ -1,6 +1,10 @@
 const participantRepo = require('../repositories/participant-repository');
 
-exports.getParticipants = async () => participantRepo.getParticipants().populate({
-    path: 'shifts',
-    options: { sort: { date: -1 } }
-});
+exports.getParticipants = async () => {
+    const participants = await participantRepo.getParticipants().populate({
+        path: 'shifts',
+        options: { sort: { date: -1 } }
+    });
+
+    return participants.sort((a, b) => b.shifts.length - a.shifts.length);
+}
